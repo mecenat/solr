33abe9d8d672b5232609b82b400c52a6817d7910
Konstantinos <konstantinos.koukouvis@mecenat.com>
2024-01-31 13:28:54 +0100

    fixed tests

--- connection_test.go
+++ connection_test.go
@@ -34,22 +34,30 @@ func TestNewConnection(t *testing.T) {
 }
 
 func TestNewRetryableConnection(t *testing.T) {
-	_, err := NewRetryableConnection("", "mycore", http.DefaultClient, 500*time.Millisecond, false)
+	conf := &RetryableConfig{
+		Timeout:      10 * time.Second,
+		RetryWaitMin: 50 * time.Millisecond,
+		RetryWaitMax: 2 * time.Second,
+		RetryMax:     4,
+		NoLog:        true,
+	}
+
+	_, err := NewRetryableConnection("", "mycore", http.DefaultClient, conf)
 	if err == nil {
 		t.Fatal("shouldn't run without a host")
 	}
 
-	_, err = NewRetryableConnection("invalid", "mycore", http.DefaultClient, 500*time.Millisecond, false)
+	_, err = NewRetryableConnection("invalid", "mycore", http.DefaultClient, conf)
 	if err == nil {
 		t.Fatal("shouldn't run without a proper host")
 	}
 
-	_, err = NewRetryableConnection("http://localhost", "", http.DefaultClient, 500*time.Millisecond, false)
+	_, err = NewRetryableConnection("http://localhost", "", http.DefaultClient, conf)
 	if err == nil {
 		t.Fatal("shouldn't run without a core defined")
 	}
 
-	c, err := NewRetryableConnection("http://localhost:8983", "mycore", http.DefaultClient, 500*time.Millisecond, false)
+	c, err := NewRetryableConnection("http://localhost:8983", "mycore", http.DefaultClient, conf)
 	if err != nil {
 		t.Fatal("shouldn't get an error but got one")
 	}
